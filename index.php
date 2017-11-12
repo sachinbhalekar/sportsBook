0b9b776bf1170f27379f3bca085d8da34c491331
jawalesumit <jawalesumit@gmail.com>
2017-11-11 17:19:17 -0800

    ok

--- index.php
+++ index.php
@@ -4,21 +4,23 @@ session_start();
 require_once 'dbconnect.php';
 
 // it will never let you open index(login) page if session is set
-if ( isset($_SESSION['user'])!="" ) {
+if ( isset($_SESSION['user'])!="" ) 
+{
     header("Location: home.php");
     exit;
 }
 
 $error = false;
 
-if( isset($_POST['btn-login']) ) {
+if( isset($_POST['login_btn']) ) 
+{
     
     // prevent sql injections/ clear user invalid inputs
-    $email = trim($_POST['email']);
+    $email = trim($_POST['username']);
     $email = strip_tags($email);
     $email = htmlspecialchars($email);
     
-    $pass = trim($_POST['pass']);
+    $pass = trim($_POST['password']);
     $pass = strip_tags($pass);
     $pass = htmlspecialchars($pass);
     // prevent sql injections / clear user invalid inputs
@@ -39,21 +41,21 @@ if( isset($_POST['btn-login']) ) {
     // if there's no error, continue to login
     if (!$error) {
         
-       $password = hash('sha256', $pass); // password hashing using SHA256
+        $password = hash('sha256', $pass); // password hashing using SHA256
         
         $res=$conn->query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
-       
+        
         $count = $res->num_rows; // if uname/pass correct it returns must be 1 row
         echo $count;
         if($count == 1 )
         {
-        while( $row=$res->fetch_assoc())
-        {
-        if($row['userPass']==$password ) {
-            $_SESSION['user'] = $row['userId'];
-            header("Location: home.php");
-        }
-        }
+            while( $row=$res->fetch_assoc())
+            {
+                if($row['userPass']==$password ) {
+                    $_SESSION['user'] = $row['userId'];
+                    header("Location: home.php");
+                }
+            }
         }
         else {
             $errMSG = "Incorrect Credentials, Try again...";
@@ -63,82 +65,69 @@ if( isset($_POST['btn-login']) ) {
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage - Login and Registration System</title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-<body>
-
-<div class="container">
-
- <div id="login-form">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
-    
-     <div class="col-md-12">
-        
-         <div class="form-group">
-             <h2 class="">Sign In.</h2>
-            </div>
-        
-         <div class="form-group">
-             <hr />
-            </div>
-            
-            <?php
-   if ( isset($errMSG) ) {
-    
-    ?>
-    <div class="form-group">
-             <div class="alert alert-danger">
-    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
-                </div>
-             </div>
-                <?php
-   }
-   ?>
-            
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-             <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
-                </div>
-                <span class="text-danger"><?php echo $emailError; ?></span>
-            </div>
-            
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-             <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
-                </div>
-                <span class="text-danger"><?php echo $passError; ?></span>
-            </div>
-            
-            <div class="form-group">
-             <hr />
-            </div>
-            
-            <div class="form-group">
-             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
-            </div>
-            
-            <div class="form-group">
-             <hr />
-            </div>
-            
-            <div class="form-group">
-             <a href="register.php">Sign Up Here...</a>
-            </div>
-        
-        </div>
-   
-    </form>
-    </div> 
-
-</div>
-
-</body>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="SportsBook">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+		<link rel="icon" href="./images/favicon.ico" type="image/ico" sizes="16x16" />
+		<title>SportsBook</title>
+		<link rel="stylesheet" type="text/css" href="./css/login.css">
+		<link href="https://fonts.googleapis.com/css?family=Zilla+Slab+Highlight" rel="stylesheet">
+	</head>
+	<body>
+		<header id="body_header">
+			<section id="header_title">
+				<h1><a href="index.html" class="a_header">SportsBook</a></h1>
+			</section>
+			<section id="header_nav_section">
+				<ul>
+					<li class="header_nav_list_item"><a href="#main_login_section" class="header_nav_list_item_a">Login</a></li>
+					<li class="header_nav_list_item"><a href="./signup/signup.html" class="header_nav_list_item_a">Sign Up</a></li>
+					<li class="header_nav_list_item"><a href="_blnak" target="_blank" class="header_nav_list_item_a">About Us</a></li>
+				</ul>
+			</section>
+		</header>
+		<section id="body_main">
+			<section id="main_intro_section">
+				<h2>Join now and never play your favorite sports alone!</h2>
+			</section>
+			<hr>
+			<section id="main_login_section">
+				<form id="login_form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+					<table id="login_table">
+						<tr>
+							<td>Username</td>
+							<td><input id="username" name="username" type="text" placeholder="Username/Email-ID" required value="<?php echo $email; ?>"/></td>
+						</tr>
+						<tr>
+							<td>Password</td>
+							<td><input id="password" name="password" type="password" required/></td>
+						</tr>
+						<tr>
+							<td colspan="2" class="td_center"><input id="login_btn" type="submit" name="login_btn" value="Login"/></td>
+						</tr>
+						<tr>
+							<td colspan="2" class="td_center">Not a member yet? <a href="./signup/signup.html">Sign Up</a></td>
+						</tr>
+					</table>
+					<?php
+                    if ( isset($errMSG) ) 
+                    {
+                    ?>
+                        <p><?php echo $errMSG; ?><p>                
+                    <?php
+                    }
+                    ?>
+				</form>
+			</section>
+		</section>
+		<footer>
+			<p>&copy; SportsBook</p>
+		</footer>
+	</body>
 </html>
 <?php ob_end_flush(); ?>
