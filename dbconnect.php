96f1c0564e085696cd5011d04d180353bac2b67d
jawalesumit <jawalesumit@gmail.com>
2017-11-11 17:26:57 -0800

    # WARNING: head commit changed in the meantime
    ok

--- dbconnect.php
+++ dbconnect.php
@@ -16,5 +16,5 @@ $conn = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 ?>
