a7e1163428c846c2d1cfec71ff4202cf889274cf
sachinbhalekar <sachinbansibhalekar@gmail.com>
2017-11-11 16:41:35 -0800

    first draft

---
+++ dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+// this will avoid mysql_connect() deprecation error.
+error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+// but I strongly suggest you to use PDO or MySQLi.
+
+define('DBHOST', 'localhost');
+define('DBUSER', 'sportsbook');
+define('DBPASS', 'sportsbook');
+define('DBNAME', 'sportsbook');
+
+
+$conn = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
