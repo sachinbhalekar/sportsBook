f772101f498ad781d13a44348faec0468b49c682
jawalesumit <jawalesumit@gmail.com>
2017-11-16 13:21:32 -0800

    style changes

--- account/home.php
+++ account/home.php
@@ -65,6 +65,7 @@ $userRow=$res->fetch_assoc();
 						<section id="all_post_section_fields">
 							<textarea id="all_post_info" name="all_post_info" maxlength="500" rows="10" placeholder=""></textarea>
 							<img id="all_post_img_loc" src="../images/location_icon.png" alt="location" />
+							<br/>
 							<span>Interested : 0</span>
 							<br/>
 							<input id="interested_btn" name="interested_btn" type="button" value="I'm interested" />
