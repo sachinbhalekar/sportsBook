bae640fe0909738343bf6acb20f6f79fed06795f
sachinbhalekar <sachinbansibhalekar@gmail.com>
2017-12-07 14:40:37 -0800

    data base conntion modified to heroku

--- connection/dbconnect.php
+++ connection/dbconnect.php
@@ -4,10 +4,10 @@
 error_reporting( ~E_DEPRECATED & ~E_NOTICE );
 // but I strongly suggest you to use PDO or MySQLi.
 
-define('DBHOST', 'localhost');
-define('DBUSER', 'sportsbook');
-define('DBPASS', 'sportsbook');
-define('DBNAME', 'sportsbook');
+define('DBHOST', 'us-cdbr-iron-east-05.cleardb.net');
+define('DBUSER', 'bf02b57bc5e498');
+define('DBPASS', '6dc188ed');
+define('DBNAME', 'heroku_2f106c6b6cd20ee');
 
 $conn = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
 
